ea370191face98630330b23a39f18c514485e9a0
Baliqis <sulaimabaliqis@gmail.com>
2024-10-27 12:51:24 +0100

    created programs page

---
+++ src/app/programs/page.js
@@ -0,0 +1,10 @@
+'use client'
+import ProgramPage from '../../pages/homePages/programsPage/ProgramPage';
+
+export default function Home() {
+    return (
+        <>
+            <ProgramPage/>
+        </>
+    );
+}
